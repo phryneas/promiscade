cbf7281d55e37be6c5ea28bec9655c0534627a7a
Lenz Weber-Tronic <mail@lenzw.de>
2024-12-22 21:15:36 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+type Cade<T> =
+  | (ReadableStreamReadValueResult<T> & { next: Promiscade<T> })
+  | ReadableStreamReadDoneResult<undefined>;
+type Promiscade<T> = Promise<Cade<T>>;
+
+export function streamToPromiscade<T>(
+  stream: ReadableStream<T>
+): Promiscade<T> {
+  const reader = stream.getReader();
+  return (function cade(): Promiscade<T> {
+    return new Promise<Cade<T>>((resolve, reject) =>
+      reader.read().then((value) => {
+        resolve(
+          value.done
+            ? { done: true }
+            : {
+                ...value,
+                next: cade(),
+              }
+        );
+      }, reject)
+    );
+  })();
+}
+
+export function promiscadeToReadableStream<T>(
+  promiscade: Promiscade<T>
+): ReadableStream<T> {
+  return new ReadableStream<T>({
+    start(controller) {
+      const onError = controller.error.bind(controller);
+      promiscade.then(function handle(cade) {
+        if (cade.done) {
+          controller.close();
+        } else {
+          controller.enqueue(cade.value);
+          cade.next.then(handle, onError);
+        }
+      }, onError);
+    },
+  });
+}
